385f39a01b134eb00b3636e48ce681d0bdc1f720
Victor Lauton Queiroz <victorlauton2012@gmail.com>
2026-08-06 18:33:44 -0300

    Main e falso banco de dados criado

---
+++ bd.py
@@ -0,0 +1,68 @@
+albuns = [
+    {
+        'Nome': 'Addison',
+        'Artista': 'Addison Rae',
+        'Gênero': 'Pop',
+        'Ano': 2025
+    },
+    {
+        'Nome': 'Renaissance',
+        'Artista': 'Beyoncé',
+        'Gênero': 'Dance/Pop',
+        'Ano': 2022
+    },
+    {
+        'Nome': 'SOS',
+        'Artista': 'SZA',
+        'Gênero': 'R&B',
+        'Ano': 2022
+    },
+    {
+        'Nome': 'World Clique',
+        'Artista': 'Deee-Lite',
+        'Gênero': 'House',
+        'Ano': 1990
+    },
+    {
+        'Nome': 'Un Verano Sin Ti',
+        'Artista': 'Bad Bunny',
+        'Gênero': 'Reggaeton',
+        'Ano': 2022
+    },
+    {
+        'Nome': 'Hit Me Hard and Soft',
+        'Artista': 'Billie Eilish',
+        'Gênero': 'Alternative/Pop',
+        'Ano': 2024
+    },
+    {
+        'Nome': 'Utopia',
+        'Artista': 'Travis Scott',
+        'Gênero': 'Hip-Hop',
+        'Ano': 2023
+    },
+    {
+        'Nome': 'GUTS',
+        'Artista': 'Olivia Rodrigo',
+        'Gênero': 'Pop Rock',
+        'Ano': 2023
+    },
+    {
+        'Nome': 'The Rise and Fall of a Midwest Princess',
+        'Artista': 'Chappell Roan',
+        'Gênero': 'Pop',
+        'Ano': 2023
+    },
+    {
+        'Nome': 'Brat',
+        'Artista': 'Charli XCX',
+        'Gênero': 'Electropop',
+        'Ano': 2024
+    },
+    {
+        'Nome': 'Wor$t Girl in America',
+        'Artista': 'Slayyyter',
+        'Gênero': 'Electrorock',
+        'Ano': 2026
+    }
+]
---
+++ main.py
@@ -0,0 +1,6 @@
+from flask import Flask     #Importei o flask
+
+app = Flask(__name__)       #Instancio uma variavel qualquer (app) que será a instância do Flask
+
+app.run()                   #Começa o servidor
+
